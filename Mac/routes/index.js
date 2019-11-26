bf193de08738a07178f9ca76ccb477e50bc94490
Chonsos <50971664+Chonsos@users.noreply.github.com>
2019-11-26 23:43:41 +0100

    Update index.js
    Actualizando  index.js

--- Mac/routes/index.js
+++ Mac/routes/index.js
@@ -1,10 +1,13 @@
-const express = require('express')
-const menuCtrl = require('../controllers/menu')
+'use strict'
 
+const express = require('express')
+const OrdenCtrl = require('../controllers/menu1')
 const api = express.Router()
 
-api.get('/menu', menuCtrl.getmenu)
-
-
+api.get('/orden', OrdenCtrl.getOrdenes)
+api.get('/orden/:ordenId', OrdenCtrl.getOrden)
+api.post('/orden', OrdenCtrl.saveOrden)
+api.put('/orden/:ordenId', OrdenCtrl.updateOrden)
+api.delete('/orden/:ordenId', OrdenCtrl.deleteOrden)
 
 module.exports = api
