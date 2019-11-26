7fbde5226d9f9c35dc7f1660462eac9fcacc04b4
Chonsos <50971664+Chonsos@users.noreply.github.com>
2019-11-27 00:15:34 +0100

    Update index.js

--- Mac/routes/index.js
+++ Mac/routes/index.js
@@ -1,8 +1,12 @@
 'use strict'
 
 const express = require('express')
+const menuCtrl = require('../controllers/menu')
 const OrdenCtrl = require('../controllers/menu1')
 const api = express.Router()
+
+// rutas menu
+api.get('/menu', menuCtrl.getmenu)
 
 api.get('/orden', OrdenCtrl.getOrdenes)
 api.get('/orden/:ordenId', OrdenCtrl.getOrden)
