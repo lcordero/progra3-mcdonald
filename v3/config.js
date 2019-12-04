f5a94b01662a89fa00621d116b648baa8641a99d
Jairo Vargas Valverde <jcvv8607@yahoo.es>
2019-12-04 16:53:40 -0600

    Mac

--- v3/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  port: process.env.PORT || 3001,
-  db: process.env.MONGODB_URI || 'mongodb+srv://jairo11:stefanny11@cluster0-orxmw.mongodb.net/test?retryWrites=true&w=majority',
-  SECRET_TOKEN: 'miclavedetokens'
-}
